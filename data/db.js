5b46e85fc6a9047b674cb45a8c4a9d19af533341
Ray Lomeli <raylom96@gmail.com>
2019-02-14 12:38:45 -0600

    features added

--- data/db.js
+++ data/db.js
@@ -1,5 +1,8 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const config = require('../knexfile.js').development;
+const db = process.env.DB_ENV || "deployment";
+const config = require("../knexfile.js")[db];
+
+// const config = require("../knexfile.js").development;
 
 module.exports = knex(config);
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const server = require('./api/server.js');
+require("dotenv").config();
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// console.log("\n Greetings:", process.env.GREET);
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
